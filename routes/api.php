f7f13f0a429d0f09ed4712514aa9cc71c5ad6510
Arduanov Danil <rylezz@gmail.com>
2025-09-30 10:54:37 +0500

    Delete user model

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\v1\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return '$request->user()';
-    })
-    //->middleware('auth:sanctum')
-    ;
-
-    Route::get('/tasks', [TaskController::class, 'index']);
-    Route::put('/tasks/{task}', [TaskController::class, 'update']);
-
     Route::post('/tasks', [TaskController::class, 'store']);
-
+    Route::get('/tasks', [TaskController::class, 'index']);
     Route::get('/tasks/{task}', [TaskController::class, 'show']);
+    Route::put('/tasks/{task}', [TaskController::class, 'update']);
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
-
-
-
 });
